16e92912edcf3d3a318ef9e999de25e802e5f07e
MohammedDayyan <dayyan.soherwardi123@gmail.com>
2026-02-25 15:35:48 +0530

    Format and clean up agent.py code

--- app/agent.py
+++ app/agent.py
@@ -2,7 +2,7 @@ from groq import Groq
 from app.prompt_templates import SQL_PROMPT
 import streamlit as st
 from app.schema import TAB
-load_dotenv()
+
 
 client = Groq(api_key=st.secrets("GROQ_API_KEY"))
 
@@ -16,4 +16,5 @@ def generate_sql(question: str):
     )
 
     return response.choices[0].message.content.strip()
+
 
